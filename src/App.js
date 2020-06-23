d630137af5fdd508d146a4de2468bbdeb969022c
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2020-06-24 02:12:19 +0530

    Listening button event

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import './App.css';
 import Person from "./Person/Person"
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "sankar", age: 28},
+      {name: "sam", age: 30},
+      {name: "indra", age: 24},
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log("Was Clicked !!!")
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm React-app</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name="Max" age="23"/>
         <Person name="Mani" age="24"> Few more(children props) </Person>
         <Person name="Maya" age="25"/>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,12 @@
 import React from "react"
 
 const person = (props) => {
-    return (
-        <div>
-            <p>I'm a {props.name} and {props.age} years Old</p>
-    <p>{props.children}</p>
-        </div>
-
-    )
+  return (
+    <div>
+      <p>I'm a {props.name} and {props.age} years Old</p>
+      <p>{props.children}</p>
+    </div>
+  )
 }
 
 export default person
