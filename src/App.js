87f1d431e247b1b856b11c616ec4ffe69c0cb5ac
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2020-06-24 03:21:41 +0530

    Adding style | inline > hard to add hover effect though > just a try

--- src/App.js
+++ src/App.js
@@ -33,10 +33,21 @@ class App extends Component {
   }
 
   render() {
+
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm React-app</h1>
-        <button onClick={() => this.switchNameHandler("Buro")}>Switch Name</button>
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler("Buro")}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person
           click={this.switchNameHandler.bind(this, "Sankar!!!")}
