9bd9b2897738a9013888635ea0c7cce8a5df9b2d
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2020-06-24 02:44:27 +0530

    Back to Class Component for now to continue The Most fundamental way

--- src/App.js
+++ src/App.js
@@ -1,48 +1,41 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from "./Person/Person"
 
-const app = props => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
+    persons: [
+      {name: "sankar", age: 28},
+      {name: "sam", age: 30},
+      {name: "indra", age: 24},
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log("Was Clicked !!!")
+    // DON"T DO THIS WAY TO CHANGE
+    // WON"T WORK
+    // this.state.persons[0].name = "Sankar Prasad Biswas"
+    // special method
+
+    this.setState({
       persons: [
-        {name: "sankar", age: 28},
+        {name: "sankar Prasad Biswas", age: 28},
         {name: "sam", age: 30},
         {name: "indra", age: 24},
-      ],
-      other: "This value will missing if you can not adjust by hooks"
-    }) 
-    // it return always 2 element, current elem & 2nd one is function to update  the current state so re-render the function
-    // remember, it will replace the object not merge, so old data can be lost
-    console.log(personsState)
-    // just comment out the following block  | start -end & see console to check the behaviour
-    // handling "other" by hooks again -start
-        const [otherState, setOtherState] = useState({other: "this can be anything to maintain separate state"})
-        console.log(personsState, otherState)
-    // handling "other" by hooks again -end
+      ]
+    })
 
-    const switchNameHandler = () => {
-      // console.log("Was Clicked !!!")
-      // DON"T DO THIS WAY TO CHANGE
-      // WON"T WORK
-      // this.state.persons[0].name = "Sankar Prasad Biswas"
-      // special method
-    
-      setPersonsState({
-        persons: [
-          {name: "sankar Prasad Biswas", age: 28},
-          {name: "sam", age: 30},
-          {name: "indra", age: 24},
-        ]
-      })
-    }
+  }
 
+  render() {
     return (
       <div className="App">
         <h1>Hi, I'm React-app</h1>
-        <button onClick={switchNameHandler}>Switch Name</button>
-        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}> children props passed !!!</Person>
-        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> children props passed !!!</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement(
@@ -51,6 +44,7 @@ const app = props => {
     //     "h1",null,"Behind the above return JSX working like this, It's messy!!!"
     //   )
     // )
+  }
 }
 
-export default app
+export default App;
