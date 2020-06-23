c58f593bf4dea2209837f5858343b829d5ca3682
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2020-06-24 02:03:03 +0530

    Functional component > Props & Children props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from "./Person/Person"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm React-app</h1>
+        <Person name="Max" age="23"/>
+        <Person name="Mani" age="24"> Few more(children props) </Person>
+        <Person name="Maya" age="25"/>
       </div>
     );
+    // return React.createElement(
+    //   "div", {className:"App"},
+    //   React.createElement(
+    //     "h1",null,"Behind the above return JSX working like this, It's messy!!!"
+    //   )
+    // )
   }
 }
 
