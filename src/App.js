0b6d711445f5d67bd5469e6f9cf199ef41b9c6cf
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2020-06-24 03:09:01 +0530

    Two way bin set up (hard coded for middleOne with a false warning)

--- src/App.js
+++ src/App.js
@@ -5,27 +5,30 @@ import Person from "./Person/Person"
 class App extends Component {
   state = {
     persons: [
-      {name: "sankar", age: 28},
-      {name: "sam", age: 30},
-      {name: "indra", age: 24},
+      { name: "sankar", age: 28 },
+      { name: "sam", age: 30 },
+      { name: "indra", age: 24 },
     ]
   }
 
   switchNameHandler = (name) => {
-    // console.log("Was Clicked !!!")
-    // DON"T DO THIS WAY TO CHANGE
-    // WON"T WORK
-    // this.state.persons[0].name = "Sankar Prasad Biswas"
-    // special method
-
     this.setState({
       persons: [
-        {name: name, age: 28},
-        {name: "sam", age: 30},
-        {name: "indra", age: 24},
+        { name: name, age: 28 },
+        { name: "sam", age: 30 },
+        { name: "indra", age: 24 },
       ]
     })
+  }
 
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "Sankar", age: 28 },
+        { name: event.target.value, age: 30 },
+        { name: "indra", age: 24 },
+      ]
+    })
   }
 
   render() {
@@ -33,9 +36,13 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm React-app</h1>
         <button onClick={() => this.switchNameHandler("Buro")}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person click={this.switchNameHandler.bind(this, "Sankar!!!")} name={this.state.persons[1].name} age={this.state.persons[1].age}> children props passed !!!</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person
+          click={this.switchNameHandler.bind(this, "Sankar!!!")}
+          changed={this.nameChangeHandler}
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}> Special One !!!</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement(
