856198e11bd9f063892b1f1a364f84bec77ddf67
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2020-06-26 02:06:49 +0530

    Some more dynamic style (red and bold if person count reduces)

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ class App extends Component {
   }
 
   nameToggleHandler = () => {
-    console.log("toggle")
     const flag = this.state.displayFlag
     this.setState({ displayFlag: !flag })
   }
@@ -70,14 +69,21 @@ class App extends Component {
           })}
         </div>
       )
-
       // change color
       style.backgroundColor = "red";
     }
 
+    let classes = []
+    if(this.state.persons.length <= 2){
+      classes.push("red")
+    }
+    if(this.state.persons.length <= 1){
+      classes.push("bold")
+    }
     return (
       <div className="App">
         <h1>Hi, I'm React-app</h1>
+        <p className={classes.join(" ")}>This is working!!!</p>
         <button
           style={style}
           onClick={this.nameToggleHandler}
