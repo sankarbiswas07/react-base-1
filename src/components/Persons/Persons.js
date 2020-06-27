fa12c5dec88ebc753956d9cf8bf1ae44f8b034ce
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2020-06-28 02:45:20 +0530

    shouldComponentUpdate use case to close the process of unwanter render - optiomization

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -13,8 +13,11 @@ class Persons extends Component {
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    console.log("Persons.js > shouldComponentUpdate");
-    return true;
+      if(nextProps.persons !== this.props.persons){
+        console.log("Persons.js > shouldComponentUpdate");
+        return true;
+      }
+      return false
   }
 
   getSnapshotBeforeUpdate(preProps, preState) {
